e1569ae52274bedce87c3f3635f041fd0731dd55
Azat Sultanov <sultazat@gmail.com>
2020-05-12 00:43:29 +0300

    refactoring

--- src/main.py
+++ src/main.py
@@ -6,20 +6,12 @@ import logging
 
 import torch
 import numpy as np
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from pytorch_pretrained_bert import BertTokenizer, BertForMaskedLM
 from keras.preprocessing.sequence import pad_sequences
-import requests
 from pytorch_pretrained_bert import BertForSequenceClassification
 from hunspell import Hunspell
 from difflib import SequenceMatcher
 import en_core_web_sm
-
-### ML BEGINNING
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# TODO decomment
-# n_gpu = torch.cuda.device_count()
-# print(torch.get_device_name(0))
 
 tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
 
@@ -32,7 +24,7 @@ def check_GE(sents):
     :rtype: (boolean, (float, float))
     """
 
-    # Create sentence) and label lists
+    # Create sentence and label lists
     # We need to add special tokens at the beginning and end of each sentence
     # for BERT to work properly
 
@@ -99,10 +91,6 @@ def check_GE(sents):
     return flat_predictions, prob_vals
 
 
-#
-# CREDIT: https://stackoverflow.com/a/39225039
-#
-
 def progress_bar(some_iter):
     try:
         from tqdm import tqdm
@@ -110,40 +98,6 @@ def progress_bar(some_iter):
     except ModuleNotFoundError:
         return some_iter
 
-
-def download_file_from_google_drive(id, destination):
-    print("Trying to fetch {}".format(destination))
-
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                return value
-
-        return None
-
-    def save_response_content(response, destination):
-        CHUNK_SIZE = 32768
-
-        with open(destination, "wb") as f:
-            for chunk in progress_bar(response.iter_content(CHUNK_SIZE)):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-
-# download_file_from_google_drive("1al7v87aRxebSUCXrN2Sdd0jGUS0zZ3vn", "./bert-based-uncased-GED.pth")
 
 modelGED = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
 
@@ -157,9 +111,6 @@ model = BertForMaskedLM.from_pretrained('bert-large-uncased')
 
 tokenizerLarge = BertTokenizer.from_pretrained('bert-large-uncased')
 
-# download the gn_GB dictionary for hunspell
-# download_file_from_google_drive("1jC5BVF9iZ0gmRQNmDcZnhfFdEYv8RNok", "./en_GB-large.dic")
-# download_file_from_google_drive("1g8PO8kdw-YmyOY_HxjnJ5FfdJFX4bsPv", "./en_GB-large.aff")
 gb = Hunspell("en_GB-large", hunspell_data_dir=".")
 
 # List of common determiners
@@ -178,20 +129,9 @@ helping_verbs = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'be',
                  'have', 'has', 'had', 'do', 'does', 'did', 'will', 'would',
                  'shall', 'should', 'may', 'might', 'must', 'can', 'could']
 
-# test sentences
-org_text = []
-# org_text.append("They drank the pub .")
-# org_text.append("I am looking forway to see you soon .")
-# org_text.append("The cat sat at mat .")
-# org_text.append("Giant otters is an apex predator .")
-# org_text.append('There is no a doubt, tracking system has brought many benefits in this information age .')
-org_text.append('I buy a book yesterday .')
 
-
+# Create a set of sentences which have possible corrected spellings
 def create_spelling_set(org_text):
-    """ Create a set of sentences which have possible corrected spellings
-    """
-
     sent = org_text
     sent = sent.lower()
     sent = sent.strip().split()
@@ -236,12 +176,8 @@ def create_spelling_set(org_text):
     return spelling_sentences
 
 
+# Create a new set of sentences with deleted determiners, prepositions & helping verbs
 def create_grammar_set(spelling_sentences):
-    """ create a new set of sentences with deleted determiners,
-        prepositions & helping verbs
-
-    """
-
     new_sentences = []
 
     for text in spelling_sentences:
@@ -270,11 +206,10 @@ def create_grammar_set(spelling_sentences):
     return spelling_sentences
 
 
+# For each input sentence create 2 sentences:
+# (1) [MASK] each word
+# (2) [MASK] for each space between words
 def create_mask_set(spelling_sentences):
-    """For each input sentence create 2 sentences
-       (1) [MASK] each word
-       (2) [MASK] for each space between words
-    """
     sentences = []
 
     for sent in spelling_sentences:
@@ -297,18 +232,13 @@ def create_mask_set(spelling_sentences):
     return sentences
 
 
+# Check grammar
 def check_grammar(org_sent, sentences, spelling_sentences):
-    """ check grammar for the input sentences
-    """
-
-    n = len(sentences)
-
     # what is the tokenized value of [MASK]. Usually 103
     text = '[MASK]'
     tokenized_text = tokenizerLarge.tokenize(text)
     mask_token = tokenizerLarge.convert_tokens_to_ids(tokenized_text)[0]
 
-    LM_sentences = []
     new_sentences = []
     i = 0  # current sentence number
     l = len(org_sent.strip().split()) * 2  # l is no of sentencees
@@ -342,76 +272,50 @@ def check_grammar(org_sent, sentences, spelling_sentences):
         predicted_index = torch.argmax(predictions[0, mask_index]).item()
         predicted_token = tokenizerLarge.convert_ids_to_tokens([predicted_index])[0]
 
-        # second best prediction. Can you used to create more options
-        #     second_index = torch.topk(predictions[0, mask_index], 2).indices[1].item()
-        #     second_prediction = tokenizer.convert_ids_to_tokens([second_index])[0]
-
         text = sent.strip().split()
         mask_index = text.index('[MASK]')
 
         if not mask:
             # case of MASKed words
-
             mask = True
             text[mask_index] = predicted_token
             try:
                 # retrieve original word
                 org_word = spelling_sentences[i // l].strip().split()[mask_index - 1]
-            #         print(">>> " + org_word)
             except:
-                #         print(spelling_sentences[i%l - 1])
-                #         print(tokenized_text)
-                #         print("{0} {1} {2}".format(i, l, mask_index))
                 print("!", end="")
                 continue
-            #     print("{0} - {1}".format(org_word, predicted_token))
-            # check if the prediction is an inflection of the original word
-            #   if org_word.isalpha() and predicted_token not in gb_infl[org_word]:
-            #     continue
+
             # use SequenceMatcher to see if predicted word is similar to original word
             if SequenceMatcher(None, org_word, predicted_token).ratio() < 0.6:
-                if org_word not in list(set(det + prep + helping_verbs)) or predicted_token not in list(
-                        set(det + prep + helping_verbs)):
+                if org_word not in list(set(det + prep + helping_verbs)) \
+                        or predicted_token not in list(set(det + prep + helping_verbs)):
                     continue
+
             if org_word == predicted_token:
                 continue
         else:
             # case for MASKed spaces
-
             mask = False
-            #     print("{0}".format(predicted_token))
+
             # only allow determiners / prepositions  / helping verbs in spaces
             if predicted_token in list(set(det + prep + helping_verbs)):
                 text[mask_index] = predicted_token
             else:
                 continue
 
-        #   if org_word == "in":
-        #     print(">>>>>> " + predicted_token)
-        #   print(tokenized_text)
-        #   print(mask_index)
-
         text.remove('[SEP]')
         text.remove('[CLS]')
         new_sent = " ".join(text)
 
-        #   print(new_sent)
-        # retain new sentences which have a
-        # minimum chance of correctness using BERT GED
+        # retain new sentences which have a minimum chance of correctness using BERT GED
         no_error, prob_val = check_GE([new_sent])
         exps = [np.exp(i) for i in prob_val[0]]
         sum_of_exps = sum(exps)
         softmax = [j / sum_of_exps for j in exps]
         if no_error and softmax[1] > 0.996:
-            #     print(org_word)
-            #     print(predicted_token)
-            #     print(SequenceMatcher(None, org_word, predicted_token).ratio())
-            #     print("{0} - {1}, {2}".format(prob_val[0][1], prob_val[0][0], prob_val[0][1] - prob_val[0][0]))
-
-            #     print("{0} - {1:.2f}".format(new_sent, softmax[1]*100) )
             print("*", end="")
             new_sentences.append(new_sent)
-    #   print("{0}\t{1}".format(predicted_token, second_prediction))
 
     print("")
 
@@ -439,8 +343,10 @@ standart_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=
 admin_markup = telegram.ReplyKeyboardMarkup(admin_keyboard, resize_keyboard=True)
 client_markup = telegram.ReplyKeyboardMarkup(client_keyboard, resize_keyboard=True)
 
+
 def first_letter_upper(s):
     return s[0].upper() + s[1:]
+
 
 def start(update, context):
     update.message.reply_text(bot_messages.start_command_response, reply_markup=standart_markup)
@@ -524,6 +430,7 @@ def ml_part(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=corrected_text)
 
     return ConversationHandler.END
+
 
 # FOR DEBUGING
 def ping(update, context):
